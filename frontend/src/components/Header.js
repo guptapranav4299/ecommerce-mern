f47d47cae9888b002f9df81c1499bb4ba84d2cbf
Pranav Gupta <guptapranav4299@gmail.com>
2021-01-10 01:08:23 +0530

    added ProductScreen component

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import {LinkContainer} from 'react-router-bootstrap'
 import { Navbar, Nav, Container } from 'react-bootstrap'
 
 
@@ -7,16 +8,22 @@ const Header = () => {
         <header>
         <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
           <Container>
+              <LinkContainer to='/'>
               <Navbar.Brand>ProShop</Navbar.Brand>
+              </LinkContainer>
             <Navbar.Toggle aria-controls='basic-navbar-nav' />
             <Navbar.Collapse id='basic-navbar-nav'>
               <Nav className='ml-auto'>
-                  <Nav.Link href='/'>
+                  <LinkContainer to='/cart'>
+                  <Nav.Link>
                   <i className='fas fa-shopping-cart'></i> Cart
                   </Nav.Link>
-                  <Nav.Link href='/'>
+                  </LinkContainer>
+                  <LinkContainer to='/login'>
+                  <Nav.Link>
                   <i className='fas fa-user'></i> Sign In
-                  </Nav.Link>        
+                  </Nav.Link>
+                  </LinkContainer>        
               </Nav>
             </Navbar.Collapse>
           </Container>
