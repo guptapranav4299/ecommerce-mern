40b5d6b6224f77923a59867e90efda67fe794a6a
Pranav Gupta <guptapranav4299@gmail.com>
2021-01-10 02:45:44 +0530

    added es module syntax in backend

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
-const express =require('express')
+import express from 'express'
+import dotenv from 'dotenv'
+import products from './data/products.js'
 const app=express()
-const products=require('./data/products')
+dotenv.config()
 app.get('/',(req,res)=>{
     res.send('API is running')
 })
@@ -14,5 +16,6 @@ app.get('/api/products/:id',(req,res)=>{
     res.json(product)
 })
 
-app.listen(5000, console.log('Server running on 5000'))
+const PORT=process.env.PORT || 5000
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
 
