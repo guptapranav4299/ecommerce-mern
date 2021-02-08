51c6a95a464ae4bc4e4b638964f6190677d14f55
Pranav Gupta <guptapranav4299@gmail.com>
2021-02-09 02:03:35 +0530

    added paypal payment integration

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,9 +1,12 @@
 import express from 'express'
 const router =express.Router()
-import { addOrderItems, getOrderbyId } from '../controllers/orderController.js'
+import { addOrderItems, getOrderbyId, updateOrderToPaid } from '../controllers/orderController.js'
 import { protect } from '../middleware/authMiddleware.js'
 
 router.route('/').post(protect, addOrderItems)
 
 router.route('/:id').get(protect, getOrderbyId)
+
+
+router.route('/:id/pay').put(protect, updateOrderToPaid)
 export default router
