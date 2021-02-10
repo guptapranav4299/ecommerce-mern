f24557511e4466d1da2db952cc8eb165addb33df
Pranav Gupta <guptapranav4299@gmail.com>
2021-02-10 23:44:34 +0530

    created product list screen for admin

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,6 +14,7 @@ import PlaceOrderScreen from './screens/PlaceOrderScreen'
 import OrderScreen from './screens/OrderScreen'
 import UserListScreen from './screens/UserListScreen'
 import UserEditScreen from './screens/UserEditScreen'
+import ProductListScreen from './screens/ProductListScreen'
 import {BrowserRouter as Router, Route } from 'react-router-dom'
 const App= () =>{
   return (
@@ -31,6 +32,7 @@ const App= () =>{
         <Route path='/' component={HomeScreen} exact></Route>
         <Route path='/product/:id' component={ProductScreen}></Route>
         <Route path='/admin/userlist' component={UserListScreen}></Route>
+        <Route path='/admin/productlist' component={ProductListScreen}></Route>
         <Route path='/admin/user/:id/edit' component={UserEditScreen}></Route>
         <Route path='/cart/:id?' component={cartScreen}></Route>
       </Container>
